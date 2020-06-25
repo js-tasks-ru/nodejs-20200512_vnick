d4cee2bbe405036fe6c161f54252d119b51bf230
vyprik <info@mylocalhost.ru>
2020-06-25 17:55:35 +0700

    add solution 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -6,12 +6,27 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
+const { on } = require('process');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get('/subscribe', (ctx, next) => {
+    return new Promise((resolve, reject) => {
+        app.once('publish', (data) => {
+            ctx.body = data.message;
+            return resolve();
+        });
+    });
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', (ctx, next) => {
+    if(Object.keys(ctx.request.body).length > 0){
+        // console.log(`* body: ${JSON.stringify(ctx.request.body)} *`)
+        app.emit('publish', ctx.request.body);
+        ctx.response.body = JSON.stringify({ status: 'ok' });
+    }else{
+        // console.log('* empty request *');
+        // ctx.response.body = JSON.stringify({ status: 'error' });
+    }
 });
 
 app.use(router.routes());
